4de899349a9ea77a8649c68080b869f0eff0e22e
kasprzyk <piotkas1@gmail.com>
2021-04-01 17:22:05 +0200

    adding new projects

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -17,7 +17,22 @@ export default {
     };
   },
   methods: {
-    handleSubmit() {}
+    handleSubmit() {
+      let project = {
+        title: this.title,
+        details: this.details,
+        complete: false
+      };
+      fetch('http://localhost:3000/projects', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(project)
+      })
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch(err => console.log(err));
+    }
   }
 };
 </script>
