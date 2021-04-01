442f5c33884445264741a47ecf2da7ff18afdb1b
kasprzyk <piotkas1@gmail.com>
2021-04-01 17:39:40 +0200

    fix

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <nav class="main-nav">
     <router-link :to="{ name: 'Home' }">Projects</router-link>
     <router-link :to="{ name: 'AddProject' }">Add a new project</router-link>
+    <router-view />
   </nav>
 </template>
 
